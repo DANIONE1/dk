0d7ffc631687e9ec53a6cb979bf24851585627f4
DaN_N <94032421+DANIONE1@users.noreply.github.com>
2023-10-05 11:14:38 -0600

    Update and rename app.py to mensaje.py
    -..

--- mensaje.py
+++ mensaje.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"message": "EN FUNCIONAMIENTO, NICE"})
+    return jsonify({"message": "EN FUNCIONAMIENTO, EXCELENTE"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
