43bd4cd548b37708a114fc8e1263600e81389a55
DaN_N <94032421+DANIONE1@users.noreply.github.com>
2023-10-06 13:23:57 -0600

    Update install_docker.sh
    777

--- install_docker.sh
+++ install_docker.sh
@@ -4,7 +4,26 @@
 sudo apt-get update
 
 # Instalar Docker
-sudo apt-get install -y docker.io
+sudo apt-get install -y docker.io#!/bin/bash
+
+# Actualiza los paquetes
+apt-get update
+
+# Instala dependencias
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+
+# Descarga la clave GPG de Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+# Agrega el repositorio de Docker
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# Actualiza nuevamente los paquetes
+apt-get update
+
+# Instala Docker
+apt-get install -y docker-ce
+
 
 # Iniciar y habilitar Docker
 sudo systemctl start docker
